71939995750a821b45b38bcda33704a2967e9ce7
Derek Yarnell <derek@umiacs.umd.edu>
2015-12-30 23:04:52 -0500

    Implementing version.

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 
+import re
 from distutils.core import setup
 
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+with open('actiontecssl/__init__.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
+
+
 setup(name='python-actiontecssl',
-      version='1.0',
+      version=version,
       description='Actiontec API for SSL Telnet',
       author='Derek Yarnell',
       author_email='derek@umiacs.umd.edu',
